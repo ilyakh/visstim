9c9020e99830d48a7f6aab97af0a46b9936491a5
Alex Brown <abrown@nimr.mrc.ac.uk>
2012-07-02 12:03:30 +0100

    Minor update: Maximally different directions script bug fix (for 2 or 3 directions, unlikely to ever actually be used but there we go...)

--- maximallyDifferentDirections.m
+++ maximallyDifferentDirections.m
@@ -6,10 +6,10 @@ directions(1) = 0;
 
 switch n
     case 2
-        directions(2) = 2;
+        directions(2) = 1;
     case 3
-        directions(2) = 2;
-        directions(3) = 3;
+        directions(2) = 1;
+        directions(3) = 2;
     otherwise
         jump180 = floor (n/2);
         for i=2:n
