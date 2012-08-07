e3584c4f20b01f6f3a280fa62b15ad6abd2e8aae
Alex Brown <abrown@nimr.mrc.ac.uk>
2012-08-07 12:06:58 +0100

    Bugfix to initialisedio routine to accept input args

--- helperFunctions/initialisedio.m
+++ helperFunctions/initialisedio.m
@@ -3,7 +3,7 @@ function [dio_input_channel] = initialisedio(q)
 %If in testing mode, skip DAQ intialisation and return an empty string
 if ~q.testingMode
     dio = digitalio('nidaq', q.deviceName);
-    dio_input_channel = addline(dio, inputLine, inputPort, 'in');
+    dio_input_channel = addline(dio, q.inputLine, q.inputPort, 'in');
 else
     dio_input_channel = '';
 end
