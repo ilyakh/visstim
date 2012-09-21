defbd4c8d0e77431101050d4bdc5eeb821b178ae
Alex Brown <abrown@nimr.mrc.ac.uk>
2012-09-21 11:36:42 +0100

    initScreen now with option for antialiasing

--- helperFunctions/initScreen.m
+++ helperFunctions/initScreen.m
@@ -1,6 +1,9 @@
-function [window,screenRect,ifi,whichScreen]=initScreen
+function [window,screenRect,ifi,whichScreen]=initScreen(multisampling)
 whichScreen = max(Screen('Screens'));
-%whichScreen = 1;
-[window,screenRect] = Screen(whichScreen, 'OpenWindow');
-    Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    ifi=Screen('GetFlipInterval', window);
+if nargin<1
+    [window,screenRect] = Screen(whichScreen, 'OpenWindow');
+else
+    [window,screenRect] = Screen(whichScreen, 'OpenWindow', [], [], [], [], [], multisampling);
+end
+Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+ifi=Screen('GetFlipInterval', window);
