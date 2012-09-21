e0c17f0bd5c60eb858e57928d117711a22ff4c93
Alex Brown <abrown@nimr.mrc.ac.uk>
2012-09-21 09:38:45 +0100

    SparseNoise stimulus generation -> random size

--- helperFunctions/generateSparseStimuli.m
+++ helperFunctions/generateSparseStimuli.m
@@ -1,17 +1,17 @@
 function stimulusInfo=generateSparseStimuli(q, stimulusInfo)
 
-xLocations=q.screenRect(3)/q.spnSpotSize;
-yLocations=q.screenRect(4)/q.spnSpotSize;
-stimulusInfo.stimuliSp=zeros(xLocations, yLocations, q.nStimFrames);
+stimulusInfo.stimuliSp=zeros(q.screenRect(3), q.screenRect(4), q.nStimFrames);
+stimulusInfo.spotSizes=cell(q.nStimFrames, 1);
 prevSpots=[];
 for i=1:q.nStimFrames
     nSpots=round(q.meanSpots+q.stdSpots*randn(1));
     nSpots=max([nSpots 1]);                         %Prevent there being no or 'negative' numbers of spots
-    randomSpots=randperm(xLocations*yLocations, nSpots);
+    stimulusInfo.spotSizes{i}=q.spotSizeMean+rand(nSpots, 1)*q.spotSizeStd;
+    randomSpots=randperm(q.screenRect(3)*q.screenRect(4), nSpots);
     while sum(ismember(randomSpots, prevSpots))>0
         randomSpots=randperm(xLocations*yLocations, nSpots);
     end
-    g=zeros(xLocations, yLocations);
+    g=zeros(q.screenRect(3), q.screenRect(4));
     g(randomSpots)=(randi(2, nSpots, 1)-1)*2-1;
     stimulusInfo.stimuliSp(:, :, i)=g;
     prevSpots=randomSpots;
